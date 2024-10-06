48dbf60d61f0875133ef8b5abe14f1b069a5e1e7
Mihaela C <mchiaburu.1337@gmail.com>
2024-10-06 19:23:02 +0300

    interfaces and ICategory implemenation

---
+++ Lib.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using Lib.DataAccess.Data;
+using Lib.DataAccess.Repository.IRepository;
+using Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
--- LibWeb/Program.cs
+++ LibWeb/Program.cs
@@ -1,4 +1,6 @@
 using Lib.DataAccess.Data;
+using Lib.DataAccess.Repository;
+using Lib.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
