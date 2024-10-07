901d696343cd2cd01b940714283512fc0a912897
Mihaela C <mchiaburu.1337@gmail.com>
2024-10-07 21:45:26 +0300

    combine create and edit pages

--- LibWeb/Areas/Admin/Controllers/ProductController.cs
+++ LibWeb/Areas/Admin/Controllers/ProductController.cs
@@ -21,7 +21,7 @@ namespace LibWeb.Areas.Admin.Controllers
             return View(objProductList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
             ProductVM productVM = new()
             {
@@ -33,12 +33,19 @@ namespace LibWeb.Areas.Admin.Controllers
                 }),
                 Product = new Product()
             };
-
-            return View(productVM);
+            if(id==null || id == 0)
+            {
+                return View(productVM);
+            }
+            else
+            {
+                productVM.Product = _unitOfWork.Product.Get(u=>u.Id==id);
+                return View(productVM); 
+            }
         }
 
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
@@ -58,32 +65,6 @@ namespace LibWeb.Areas.Admin.Controllers
             }
         }
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfuly";
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
