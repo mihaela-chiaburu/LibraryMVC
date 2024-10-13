303f5faa28064e6f34cf4a74dba60b69285483e5
Mihaela C <mchiaburu.1337@gmail.com>
2024-10-13 22:24:35 +0300

    added dbtable and functionality for shopping cart

--- Lib.DataAccess/Repository/Repository.cs
+++ Lib.DataAccess/Repository/Repository.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Lib.DataAccess.Repository
 {
@@ -26,9 +27,17 @@ namespace Lib.DataAccess.Repository
             dbSet.Add(entity);
         }
 
-        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
         {
-            IQueryable<T> query = dbSet;
+            IQueryable<T> query;
+            if (tracked)
+            {
+                query = dbSet;
+            }
+            else
+            {
+                query = dbSet.AsNoTracking();
+            }
             query = query.Where(filter);
             if (!string.IsNullOrEmpty(includeProperties))
             {
@@ -38,7 +47,8 @@ namespace Lib.DataAccess.Repository
                     query = query.Include(includeProp);
                 }
             }
-            return query.FirstOrDefault(); 
+            return query.FirstOrDefault();
+
         }
 
         public IEnumerable<T> GetAll( string? includeProperties = null)
---
+++ Lib.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,25 @@
+﻿using Lib.DataAccess.Data;
+using Lib.DataAccess.Repository.IRepository;
+using Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
